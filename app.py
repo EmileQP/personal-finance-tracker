41aaf7c536211c05f7a708cac948877ce08368d6
EmileQP <149075190+EmileQP@users.noreply.github.com>
2024-08-30 00:38:24 +0200

    New boiler plate

--- app.py
+++ app.py
@@ -4,3 +4,21 @@ from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
+from helpers import login_required, lookup, usd
+
+app = Flask(__name__)
+
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///finance.db")
+
+@app.route("/")
+@login_required
+def index():
+    return render_template("index.html")
